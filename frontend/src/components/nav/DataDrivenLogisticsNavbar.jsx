198adc223ed12c1fc44319cdeec5fc0f5a2ac126
Patron <Patron@Patrons-MacBook-Air.local>
2024-02-29 14:09:25 -0600

    adjusted navbar.

--- frontend/src/components/nav/DataDrivenLogisticsNavbar.jsx
+++ frontend/src/components/nav/DataDrivenLogisticsNavbar.jsx
@@ -4,10 +4,9 @@ import { Link } from "react-router-dom";
 import logo from '../../assets/logo.png'
 
 export default function DataDrivenLogisticsNavbar(props) {
-    return <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
-        <Container>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Brand as={Link} to="/">
+    return <Navbar bg="dark" variant="dark">
+        <Container fluid>
+            <Navbar.Brand as={Link} to="/dashboard">  
                 <img
                     alt="Data Driven Logistics Logo"
                     src={logo}
