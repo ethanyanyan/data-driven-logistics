13a3d05b99f6481b78a2a7b4048ddc7b71ad1605
Lee Bo Hua <blee298@wisc.edu>
2024-03-12 16:35:11 -0500

    Basic implementation for all routes

--- backend/controllers/InventoryController.js
+++ backend/controllers/InventoryController.js
@@ -1,4 +1,10 @@
-const Items = require("./../models/inventory");
+/**
+ * InventoryController.js
+ * This controller handles all inventory-related operations, including
+ * creating, retrieving (all and by inventoryId), updating and deleting inventory,
+ */
+
+const InventoryLevel = require("./../models/InventoryLevel");
 const db = require("../config/dbConfig");
 
 // Logs a new Item in the inventory
@@ -6,13 +12,13 @@ async function createItem(req, res) {
   const { LocationID, ProductID, Quantity } = req.body;
 
   try {
-    const newItems = new Items(
+    const newInventory = new InventoryLevel(
       (InventoryLevelID = null),
       LocationID,
       ProductID,
       Quantity
     );
-    const result = await newItems.save();
+    const result = await newInventory.save();
     res.status(200).json({
       message: "Item logged successfully",
       data: result,
@@ -25,7 +31,7 @@ async function createItem(req, res) {
 // Get status and details of a particular item
 async function getInventory(req, res) {
   try {
-    const item = await Items.findByID(req.params.id);
+    const item = await InventoryLevel.findByID(req.params.id);
     if (item) {
       res.status(200).json({
         message: `Inventory id ${req.params.id} retrieved successfully`,
@@ -43,10 +49,10 @@ async function getInventory(req, res) {
   }
 }
 
-// Get all shipments
+// Get all Inventory
 async function getAllItems(req, res) {
   try {
-    const allItems = await Items.findAll();
+    const allItems = await InventoryLevel.findAll();
     res.status(200).json({
       message: "Items retrieved successfully",
       data: allItems,
@@ -58,66 +64,88 @@ async function getAllItems(req, res) {
   }
 }
 
-// // Update a shipment
-// async function updateShipment(req, res) {
-//   const updateData = {};
-//   const allowedUpdates = [
-//     "SourceID",
-//     "DestinationID",
-//     "DepartureDate",
-//     "ArrivalDate",
-//     "Status",
-//   ];
+// Update an Inventory quantity
+async function updateQuantity(req, res) {
+  const newQuantity = req.body.Quantity;
+  try {
+    const inventory = await InventoryLevel.findByID(req.params.id);
+    if (inventory) {
+      const updated = await inventory.adjustQuantity(newQuantity);
+      if (updated) {
+        res.status(200).json({
+          message: `Quantity for inventory id ${req.params.id} updated successfully`,
+          data: await InventoryLevel.findByID(req.params.id),
+        });
+      } else {
+        res.status(500).json({
+          error: "Error updating the inventory level quantity",
+        });
+      }
+    } else {
+      res.status(404).json({
+        error: `Inventory with id ${req.params.id} not found`,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "Database error occurred.",
+    });
+  }
+}
 
-//   // Collect fields to update
-//   for (const field of allowedUpdates) {
-//     if (req.body.hasOwnProperty(field)) {
-//       updateData[field] = req.body[field];
-//     }
-//   }
+// Update a Inventory details
+async function updateInventory(req, res) {
+  const updateData = {};
+  const allowedUpdates = ["LocationID", "ProductID", "Quantity"];
 
-//   try {
-//     const shipmentUpdated = await Shipment.updateShipment(
-//       req.params.id,
-//       updateData
-//     );
-//     if (shipmentUpdated) {
-//       res.status(200).json({
-//         message: `Shipment id ${req.params.id} updated successfully`,
-//         // Ok I want the return data to call getShipment
-//         data: await Shipment.findByID(req.params.id),
-//       });
-//     } else {
-//       res
-//         .status(404)
-//         .json({ error: `Shipment with id ${req.params.id} not found` });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ error: "Database error occurred." });
-//   }
-// }
+  // Collect fields to update
+  for (const field of allowedUpdates) {
+    if (req.body.hasOwnProperty(field)) {
+      updateData[field] = req.body[field];
+    }
+  }
+  try {
+    const inventoryUpdated = await InventoryLevel.update(
+      req.params.id,
+      updateData
+    );
+    if (inventoryUpdated) {
+      res.status(200).json({
+        message: `InventoryLevel id ${req.params.id} updated successfully`,
+        data: await InventoryLevel.findByID(req.params.id),
+      });
+    } else {
+      res
+        .status(404)
+        .json({ error: `InventoryLevel with id ${req.params.id} not found` });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Database error occurred." });
+  }
+}
 
-// // Delete a shipment
-// async function deleteShipment(req, res) {
-//   try {
-//     const shipment = await Shipment.findByID(req.params.id);
-//     if (shipment) {
-//       await Shipment.delete(req.params.id);
-//       res.status(200).json({
-//         message: `Shipment id ${req.params.id} deleted successfully`,
-//       });
-//     } else {
-//       res.status(404).json({
-//         error: `Shipment with id ${req.params.id} not found`,
-//       });
-//     }
-//   } catch (error) {
-//     res.status(500).json({
-//       error: "Database error occurred.",
-//     });
-//   }
-// }
+// Delete an inventory
+async function deleteInventory(req, res) {
+  try {
+    const inventory = await InventoryLevel.findByID(req.params.id);
+    if (inventory) {
+      await InventoryLevel.delete(req.params.id);
+      res.status(200).json({
+        message: `InventoryLevel id ${req.params.id} deleted successfully`,
+      });
+    } else {
+      res.status(404).json({
+        error: `InventoryLevel with id ${req.params.id} not found`,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "Database error occurred.",
+    });
+  }
+}
 
+// Temp route for testing
 async function getAllProducts(req, res) {
   try {
     const [results] = await db.pool.query("SELECT * FROM Products");
@@ -127,6 +155,7 @@ async function getAllProducts(req, res) {
   }
 }
 
+// Temp route for testing
 async function getAllLocations(req, res) {
   try {
     const [results] = await db.pool.query("SELECT * FROM Locations");
@@ -142,4 +171,7 @@ module.exports = {
   getAllLocations,
   getAllItems,
   getInventory,
+  deleteInventory,
+  updateInventory,
+  updateQuantity,
 };
--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -14,11 +14,14 @@ router.get("/:id", inventoryController.getInventory);
 // Endpoint for getting all items in the inventory
 router.get("/", inventoryController.getAllItems);
 
-// // Endpoint to update an item in the inventory
-// router.patch("/:id", inventoryController.updateShipment);
+// Endpoint to update the quantity of an item in the inventory
+router.put("/:id", inventoryController.updateQuantity);
 
-// // Endpoint to delete an item in the inventory
-// router.delete("/:id", inventoryController.deleteShipment);
+// Endpoint to update an item in the inventory
+router.patch("/:id", inventoryController.updateInventory);
+
+// Endpoint to delete an item in the inventory
+router.delete("/:id", inventoryController.deleteInventory);
 
 // (MAYBE) HIDDEN ENDPOINT to view all products in the Products table
 router.get("/products", inventoryController.getAllProducts);
