0a78483a3ef1d08e7bb5499bc4bed7a7fdaaeecc
Junaid Ackroyd <jackroyd@wisc.edu>
2024-03-18 14:03:17 -0500

    Update DataDrivenLogisticsNavbar.jsx

--- frontend/src/components/navigation/DataDrivenLogisticsNavbar.jsx
+++ frontend/src/components/navigation/DataDrivenLogisticsNavbar.jsx
@@ -1,21 +1,34 @@
-import { Container, Navbar } from "react-bootstrap";
+import { Button, Container, Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-import logo from '../../assets/images/logo.png'
+import logo from '../../assets/images/logo.png';
 
-export default function DataDrivenLogisticsNavbar(props) {
-    return <Navbar bg="dark" variant="dark">
-        <Container fluid>
-            <Navbar.Brand as={Link} to="/dashboard">  
-                <img
-                    alt="Data Driven Logistics Logo"
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />{' '}
-                Data Driven Logistics
-            </Navbar.Brand>
-        </Container>
+export default function DataDrivenLogisticsNavbar({ handleLogout }) {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/dashboard">
+          <img
+            alt="Data Driven Logistics Logo"
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Data Driven Logistics
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {/* Add any additional navigation links here */}
+          </Nav>
+          <Nav>
+            <Button variant="outline-light" onClick={handleLogout}>
+              Logout
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
+  );
 }
