f49e9b9bf70d16a01be4a8ebc96b0a432b2cecb2
Patron <Patron@Patrons-MacBook-Air-2.local>
2024-04-22 16:42:17 -0500

    leaving branch to work on fixing modal rendering.

--- frontend/src/components/navigation/DataDrivenLogisticsNavbar.jsx
+++ frontend/src/components/navigation/DataDrivenLogisticsNavbar.jsx
@@ -1,12 +1,15 @@
+import React, { useState } from "react";
 import { Container, Navbar, Nav, Image } from "react-bootstrap";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import logo from '../../assets/images/logo.png';
 import "./DataDrivenLogisticsNavbar.css";
 import BaseBtn from "./../BaseComponents/BaseBtn";
+import BaseModal from "../../components/BaseComponents/BaseModal";
 
 export default function DataDrivenLogisticsNavbar() {
   const { logout, user } = useAuth();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const navigate = useNavigate();
 
   const showPeopleTab = user && [1, 2, 4].includes(user.RoleID);
@@ -14,7 +17,7 @@ export default function DataDrivenLogisticsNavbar() {
     { path: "/shipment-tracking", name: "Shipments" },
     // Conditionally add "People" tab based on the user's role
     ...(showPeopleTab ? [{ path: "/users", name: "People" }] : []),
-    { action: logout, name: "Logout" },
+    { action: () => setModalIsOpen(true), name: "Logout" },
     // Add more links as needed
   ];
 
@@ -27,30 +30,54 @@ export default function DataDrivenLogisticsNavbar() {
     }
   };
 
+  const logoutClick = () => {
+    setModalIsOpen(false);
+    logout();
+  }
+
   return (
-    <Navbar className="navbar-custom" expand="lg">
-      <Container fluid>
-        <Navbar.Brand as={NavLink} to="/dashboard">
-          <Image alt="Data Driven Logistics Logo" src={logo} className="ddl-logo" />
-        </Navbar.Brand>
-        <Navbar.Text className="navbar-text-header">
-          Data Driven Logistics
-        </Navbar.Text>
-        <Navbar.Toggle className="navbar-toggle" aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="navbar-collapse">
-          <Nav className="ms-auto">
-            {navigationLinks.map((link, index) => (
-              <BaseBtn
-                key={index}
-                onClick={() => handleNavClick(link)}
-                label={link.name}
-                btnType="primary"
-                size="sm"
-              />
-            ))}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar className="navbar-custom" expand="lg">
+        <Container fluid>
+          <Navbar.Brand as={NavLink} to="/dashboard">
+            <Image alt="Data Driven Logistics Logo" src={logo} className="ddl-logo" />
+          </Navbar.Brand>
+          <Navbar.Text className="navbar-text-header">
+            Data Driven Logistics
+          </Navbar.Text>
+          <Navbar.Toggle className="navbar-toggle" aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="navbar-collapse">
+            <Nav className="ms-auto">
+              {navigationLinks.map((link, index) => (
+                <BaseBtn
+                  key={index}
+                  onClick={() => handleNavClick(link)}
+                  label={link.name}
+                  btnType="primary"
+                  size="sm"
+                />
+              ))}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <BaseModal
+          isOpen={modalIsOpen}
+          onRequestClose={() => setModalIsOpen(false)}
+          width="400px"
+        >
+          {{
+            header: <h2>Confirm Logout</h2>,
+            buttons: (
+              <div>
+                <span style={{ marginRight: "10px" }}>
+                  <BaseBtn onClick={() => setModalIsOpen(false)}>Cancel</BaseBtn>
+                </span>
+                <BaseBtn onClick={() => logoutClick()}>Logout</BaseBtn>
+              </div>
+            ),
+          }}
+        </BaseModal>
+      </div>
   );
 }
