97c09d9492a1b5345504193ba7454b18b85b5250
Bo Hua LEE <blee298@wisc.edu>
2024-04-22 20:26:24 -0500

    !48 Update shipment backend to integrate shipmentdetails

--- backend/controllers/ShipmentsController.js
+++ backend/controllers/ShipmentsController.js
@@ -1,4 +1,5 @@
 const Shipment = require("../models/Shipments");
+const ShipmentDetails = require("../models/ShipmentDetails");
 
 // Logs a new shipment
 async function logShipment(req, res) {
@@ -112,7 +113,7 @@ async function updateShipment(req, res) {
   try {
     const shipmentUpdated = await Shipment.updateShipment(
       req.params.id,
-      updateData,
+      updateData
     );
     if (shipmentUpdated) {
       res.status(200).json({
@@ -151,6 +152,75 @@ async function deleteShipment(req, res) {
   }
 }
 
+async function logShipmentDetails(req, res) {
+  const ShipmentID = req.params.id;
+  const { ProductID, Quantity } = req.body;
+  try {
+    const shipmentDetailsData = {
+      ShipmentID,
+      ProductID,
+      Quantity,
+    };
+
+    const newShipmentDetails = new ShipmentDetails(shipmentDetailsData);
+    const result = await newShipmentDetails.save();
+
+    res.status(200).json({
+      message: "Shipment details logged successfully",
+      data: result,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Database error occurred: " + error.message });
+  }
+}
+
+async function getShipmentDetails(req, res) {
+  try {
+    const shipmentDetails = await ShipmentDetails.findByShipmentID(
+      req.params.id
+    );
+    if (shipmentDetails) {
+      res.status(200).json({
+        message: `Shipment details for shipment id ${req.params.id} retrieved successfully`,
+        data: shipmentDetails,
+      });
+    } else {
+      res.status(404).json({
+        error: `Shipment details for shipment id ${req.params.id} not found`,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "Database error occurred.",
+    });
+  }
+}
+
+async function deleteShipmentDetails(req, res) {
+  try {
+    const shipmentDetails = await ShipmentDetails.findByID(
+      req.params.id,
+      req.params.productID
+    );
+    if (shipmentDetails) {
+      await ShipmentDetails.delete(req.params.id, req.params.productID);
+      res.status(200).json({
+        message: `Shipment details for shipment id ${req.params.id} and product id ${req.params.productID} deleted successfully`,
+      });
+    } else {
+      res.status(404).json({
+        error: `Shipment details for shipment id ${req.params.id} and product id ${req.params.productID} not found`,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "Database error occurred.",
+    });
+  }
+}
+
 module.exports = {
   logShipment,
   getShipment,
@@ -158,4 +228,7 @@ module.exports = {
   getShipmentsByBusinessId,
   updateShipment,
   deleteShipment,
+  logShipmentDetails,
+  getShipmentDetails,
+  deleteShipmentDetails,
 };
---
+++ backend/models/ShipmentDetails.js
@@ -0,0 +1,115 @@
+const db = require("../config/dbConfig");
+
+class ShipmentDetails {
+  /**
+   * Constructs a ShipmentDetails instance with detailed information.
+   *
+   * @param {object} row - An object containing all shipmentDetail properties.
+   */
+  constructor(row) {
+    this.ShipmentDetailID = row.ShipmentDetailID;
+    this.ShipmentID = row.ShipmentID;
+    this.ProductID = row.ProductID;
+    this.Quantity = row.Quantity;
+  }
+
+  /**
+   * Saves the current shipmentDetail instance to the database.
+   *
+   * @return {Promise<object>} The result object from the database operation.
+   */
+  async save() {
+    let query = `
+      SELECT * FROM ShipmentDetails 
+      WHERE ShipmentID = ? AND ProductID = ?
+    `;
+    let values = [this.ShipmentID, this.ProductID];
+
+    try {
+      const [rows] = await db.pool.query(query, values);
+
+      // If a row with the same ShipmentID and ProductID exists, update it
+      if (rows.length > 0) {
+        query = `
+          UPDATE ShipmentDetails 
+          SET Quantity = ?
+          WHERE ShipmentID = ? AND ProductID = ?
+        `;
+        values = [this.Quantity, this.ShipmentID, this.ProductID];
+      } else {
+        // Otherwise, insert a new row
+        query = `
+          INSERT INTO ShipmentDetails 
+          (ShipmentDetailID, ShipmentID, ProductID, Quantity) 
+          VALUES (?, ?, ?, ?)
+        `;
+        values = [
+          this.ShipmentDetailID,
+          this.ShipmentID,
+          this.ProductID,
+          this.Quantity,
+        ];
+      }
+
+      const [result] = await db.pool.query(query, values);
+      return result;
+    } catch (error) {
+      throw new Error("Error saving the shipmentDetail: " + error.message);
+    }
+  }
+
+  /**
+   * Finds shipmentDetails by their shipment.
+   *
+   * @param {number} ShipmentID - The shipment ID of shipmentDetails to find.
+   * @return {Promise<Array<ShipmentDetails>>} An array of ShipmentDetails instances that match the shipment.
+   */
+  static async findByShipmentID(ShipmentID) {
+    const query = `SELECT * FROM ShipmentDetails WHERE ShipmentID = ?`;
+
+    try {
+      const [rows] = await db.pool.query(query, [ShipmentID]);
+      return rows.map((row) => new ShipmentDetails(row));
+    } catch (error) {
+      throw new Error("Error finding shipmentDetails: " + error.message);
+    }
+  }
+
+  /**
+   * Finds shipmentDetails by their unique identifier of ShipmentID and ProductID.
+   * @param {number} ShipmentDetailID - The unique identifier of the shipmentDetail to find.
+   * @return {Promise<ShipmentDetails|null>} The found shipmentDetail as a ShipmentDetails instance, or null if not found.
+   */
+  static async findByID(ShipmentID, ProductID) {
+    const query = `SELECT * FROM ShipmentDetails WHERE ShipmentID = ? AND ProductID = ?`;
+
+    try {
+      const [rows] = await db.pool.query(query, [ShipmentID, ProductID]);
+      if (rows.length > 0) {
+        return new ShipmentDetails(rows[0]);
+      } else {
+        return null;
+      }
+    } catch (error) {
+      throw new Error("Error finding the shipmentDetail: " + error.message);
+    }
+  }
+
+  /**
+   * Deletes a shipmentDetail by its unique identifier.
+   *
+   * @param {number} ShipmentDetailID - The unique identifier of the shipmentDetail to delete.
+   * @return {Promise<object>} The result object from the database operation.
+   */
+  static async delete(ShipmentID, ProductID) {
+    const query = `DELETE FROM ShipmentDetails WHERE ShipmentID = ? AND ProductID = ?`;
+    try {
+      const [result] = await db.pool.query(query, [ShipmentID, ProductID]);
+      return result;
+    } catch (error) {
+      throw new Error("Error deleting the shipmentDetail: " + error.message);
+    }
+  }
+}
+
+module.exports = ShipmentDetails;
